06c08c0b3f5929ad55f45342cd0f750d1721aeb3
Pavithra Selvan <pselvan@charlotte.edu>
2026-03-02 12:13:05 -0500

    env file change to point to the new server url

--- genai_innovations/src/app/environments/environment.ts
+++ genai_innovations/src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  api_host: 'http://ec2-34-230-77-200.compute-1.amazonaws.com:3000'
+  api_host: 'http://192.168.0.55:3000'
+  // api_host: 'http://ec2-34-230-77-200.compute-1.amazonaws.com:3000'
 };
