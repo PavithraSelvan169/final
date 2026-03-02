e515b5b4b2b881b0e70caf0ca70a9954277c8f4c
Pavithra Selvan <pselvan@charlotte.edu>
2026-03-02 12:40:28 -0500

    new https url for the application

--- genai_innovations/src/app/environments/environment.ts
+++ genai_innovations/src/app/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  api_host: 'http://192.168.0.55:3000'
+  api_host: 'https://api.riseofgenai.duckdns.org'
+  //api_host: 'http://192.168.0.55:3000'
   // api_host: 'http://ec2-34-230-77-200.compute-1.amazonaws.com:3000'
 };
